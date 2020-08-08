4568643ba37777bcc9e17e181079e9ee7eb8d888
Artem Kvitka <kvitkaart554@gmail.com>
2020-08-08 18:19:00 +0300

    fix eslint problems

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
 import styled from 'styled-components';
 import Head from 'next/head';
